034918afa601ebb60c6005288f5dce3ac76ee4d8
Pavani Madarasinghe <150323526+GitMPavani@users.noreply.github.com>
2024-01-24 00:41:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import { Route, Routes } from 'react-router-dom';
 const App = () => (
   <div className='container'>
     <Routes>  {/* Changed from <switch> to <Routes> */}
-      <Route path='/' element={<Home />} />  {/* Use 'element' instead of 'component' */}
-      <Route path='/blog/:id' element={<Blog />} />  {/* Use 'element' instead of 'component' */}
+      <Route path='/blog' element={<Home />} />  {/* Use 'element' instead of 'component' */}
+      <Route path='/blog/blog/:id' element={<Blog />} />  {/* Use 'element' instead of 'component' */}
     </Routes>
   </div>
 );
